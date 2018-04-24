5eec32c17ffb1a529cd0b8fdbe1613c44f089c8e
aswinichandramohan <35481342+aswinichandramohan@users.noreply.github.com>
2018-04-24 11:53:56 -0700

    RegisterController. Adding new controller

---
+++ BookReviewSpring18/Controllers/RegisterController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookReviewSpring18.Models;
+
+namespace BookReviewSpring18.Controllers
+{
+    public class RegisterController : Controller
+    {
+        BookReviewDbEntities db = new BookReviewDbEntities(); //Reference
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "ReviewerUserName, ReviewerFirstName," +
+            "ReviewerLastName, ReviewerEmail, ReviewPlainPassword")]Reviewer r) // Bind going to bind all fields from the stored procedure to new instants of class r
+              //Reviewer here is a existing class in Models 
+        {
+            Message m = new Message();
+            int result = db.usp_NewReviewer(r.ReviewerUserName, r.ReviewerFirstName,
+            r.ReviewerLastName, r.ReviewerEmail, r.ReviewPlainPassword);
+
+            if (result != -1)
+            {
+                m.MessageText = "Welcome, " + r.ReviewerUserName;
+            }
+
+            else
+            {
+                m.MessageText = "Something Went Error";
+
+            }
+
+            return View("Result", m);
+        }
+
+        public ActionResult Result(Message m)
+        {
+            return View(m);
+        }
+
+
+
+
+    }
+}
---
+++ BookReviewSpring18/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookReviewSpring18.Models
+{
+    public class Message
+    {
+        public Message() { }// constructor initialize value of class & constructor should be name as class name 
+
+        public Message(string msg)
+        {
+            MessageText = msg;
+        }
+        public string MessageText { set; get; }
+    }
+}
